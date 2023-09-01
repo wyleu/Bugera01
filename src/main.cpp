ce69a46a40cb22809c8aeae60e06504d70acf90e
wyleu <wyleus@gmail.com>
2023-09-01 13:10:58 +0100

    Debounce

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,6 @@ USBMIDI midi;
 
 #define ledPin PC13 //13
 
-
-
 void setup() {
   USBComposite.clear();
   USBComposite.setProductId(0x0067);
@@ -44,17 +42,33 @@ boolean bluestate = false;
 boolean blackstate = false;
 boolean whitestate = false;
 
-boolean check(int colour, boolean state, int value){
-    if (digitalRead(colour) == LOW and state == false) {
-      state = true;
-      midi.sendControlChange(15, value, 127);
-      digitalWrite(ledPin, LOW);
-    }
+unsigned long redwait = 0;
+unsigned long greenwait = 0;
+unsigned long yellowwait = 0;
+unsigned long bluewait = 0;
+unsigned long blackwait = 0;
+unsigned long whitewait = 0;
 
-    if (digitalRead(colour) == HIGH and state == true) {
-      state = false;
-      midi.sendControlChange(15, value, 0);
-      digitalWrite(ledPin, HIGH);
+unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
+unsigned long debounceDelay = 50; // the debounce time; increase if the output flickers
+
+boolean check(int colour, boolean state, int value, unsigned long *wait){
+
+    if ((millis() - *wait) > debounceDelay) {
+
+      if (digitalRead(colour) == LOW and state == false) {
+        state = true;
+        midi.sendControlChange(15, value, 127);
+        digitalWrite(ledPin, LOW);
+        *wait = millis();
+      }
+
+      if (digitalRead(colour) == HIGH and state == true) {
+        state = false;
+        midi.sendControlChange(15, value, 0);
+        digitalWrite(ledPin, HIGH);
+        *wait = millis();
+      }
     }
 
     return state;
@@ -63,17 +77,15 @@ boolean check(int colour, boolean state, int value){
 
 void loop() {
 
-  redstate = check(PA9, redstate, 102);
-  greenstate = check(PA8, greenstate, 103);
-  yellowstate = check(PB15, yellowstate, 104);
-  bluestate = check(PB14, bluestate, 105);
-  blackstate = check(PB13, blackstate, 106);
-  whitestate = check(PB12, whitestate, 107);
+  redstate = check(PA9, redstate, 102, &redwait);
+  greenstate = check(PA8, greenstate, 103, &greenwait);
+  yellowstate = check(PB15, yellowstate, 104, &yellowwait);
+  bluestate = check(PB14, bluestate, 105, &bluewait);
+  blackstate = check(PB13, blackstate, 106, &blackwait);
+  whitestate = check(PB12, whitestate, 107, &whitewait);
 
   // digitalWrite(ledPin, HIGH);
   // delay(1000);
   // digitalWrite(ledPin, LOW);
   // delay(1000);
 }
-
-
